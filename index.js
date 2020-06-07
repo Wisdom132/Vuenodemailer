2e78ab479e8e41830164c311fba8b97c4be74f15
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-07 20:58:20 +0100

    setup main file for express

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Vue Mailer Application 😎😎 '
+    });
+});
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
