8b08bc7736f13b0c5cf4d1059be97dd2b11c34f1
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-08 01:02:30 +0100

    setup handlebars

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -13,3 +13,18 @@ let transporter = nodemailer.createTransport({
         rejectUnauthorized: false
     }
 });
+
+
+
+const handlebarOptions = {
+    viewEngine: {
+        extName: '.handlebars',
+        partialsDir: 'server/views/patials',
+        layoutsDir: 'server/views/layouts',
+        defaultLayout: '',
+    },
+    viewPath: 'server/views/templates',
+    extName: '.handlebars',
+};
+
+transporter.use('compile', hbs(handlebarOptions));
