3e788f5d29268b60c15ba0e420c90106ba769238
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-09 18:31:41 +0100

    main functionality working now

--- client/index.js
+++ client/index.js
@@ -1,6 +1,27 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
+        user: {
+            name: "",
+            email: "",
+            phone: ""
+        },
+        loader: false
+    },
+    methods: {
+        async registerUser() {
+            this.loader = true
+            try {
+                debugger
+                let response = await axios.post('http://localhost:4000/user', this.user);
+
+                console.log(response)
+                alert("Registration Was successful");
+                this.loader = false;
+            } catch (err) {
+                alert("Something Went Wrong");
+                this.loader = false
+            }
+        }
+    },
 })
